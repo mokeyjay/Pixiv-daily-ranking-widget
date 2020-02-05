4d7e68191cebfb97d4ddc17583394b6f289e09e0
mokeyjay <m@html.love>
2020-02-05 19:13:33 +0800

    使用官方ajax接口获取排行榜；从原先的插画排行榜改为综合排行榜

--- app/Libs/Pixiv.php
+++ app/Libs/Pixiv.php
@@ -10,31 +10,84 @@ namespace app\Libs;
 class Pixiv
 {
     /**
-     * 获取Pixiv图片url列表
-     * @return array|false
+     * 调用官方ajax接口获取排行榜数据
+     * @param int $page 页码，最多10页
+     * @return mixed
+     * @throws \Exception
      */
-    public static function getImageList()
+    public static function getRanking($page = 1)
     {
-        $imageList = Storage::getJson('source', true);
-        if (is_array($imageList)) {
-            return $imageList;
-        }
-
-        $html = Curl::get('https://www.pixiv.net/ranking.php?mode=daily&content=illust');
-        preg_match_all('|https://i\.pximg\.net/c/240x480/img-master/img/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}/\d{2}/.*?\.\w{3}|', $html, $image); // 匹配缩略图url
-        preg_match_all('|<a href="/(artworks/\d+)"class="title"|', $html, $url); // 匹配链接
-
-        if (empty($image[0]) || empty($url[1])){
+        $response = Curl::get("https://www.pixiv.net/ranking.php?mode=daily&p={$page}&format=json", [
+            CURLOPT_HTTPHEADER => [
+                'Referer: https://www.pixiv.net/ranking.php?mode=daily',
+            ],
+        ]);
+        $json = json_decode($response, true);
+        if (!isset($json['contents'])) {
+            Tools::log('获取排行榜数据失败！接口返回值：' . $response, Tools::LOG_LEVEL_ERROR);
             return false;
         }
 
-        $json = [
-            'image' => $image[0],
-            'url'   => $url[1],
-        ];
-        Storage::saveJson('source', $json);
-
         return $json;
+    }
+
+    /**
+     * 检查排行榜是否已更新
+     * @return bool
+     * @throws \Exception
+     */
+    public static function checkRankingUpdate()
+    {
+        $json = self::getRanking();
+
+        if ($json && isset($json['date']) && preg_match('|^\d{8}$|', $json['date'])) {
+            return date('Y-m-d', strtotime($json['date'])) === date('Y-m-d');
+        }
+
+        throw new \Exception('检查排行榜更新失败！接口返回值：' . json_encode($json));
+    }
+
+    /**
+     * 获取图片url列表
+     * @return array|false
+     * @throws \Exception
+     */
+    public static function getImages()
+    {
+        $source = Storage::getJson('source', true);
+        if (is_array($source)) {
+            return $source;
+        }
+
+        // 兼容旧格式
+        $source = [
+            'image' => [],
+            'url'   => [],
+        ];
+
+        $picNum = 0;
+        for ($page = 1; $page <= 10; $page++) {
+
+            $json = self::getRanking($page);
+            if($json === false){
+                return false;
+            }
+
+            foreach ($json['contents'] as $item) {
+                $source['image'][] = $item['url'];
+                $source['url'][] = "artworks/{$item['illust_id']}";
+                $picNum++;
+
+                if ($picNum >= Config::$limit) {
+                    break 2;
+                }
+            }
+        }
+
+        $source['date'] = date('Y-m-d', strtotime($json['date']));
+        Storage::saveJson('source', $source);
+
+        return $source;
     }
 
     /**
@@ -50,10 +103,11 @@ class Pixiv
         if ($image == false) {
             $image = Curl::get($url, [
                 CURLOPT_HTTPHEADER => [
-                    'Referer: https://www.pixiv.net/ranking.php?mode=daily&content=illust',
+                    'Referer: https://www.pixiv.net/ranking.php?mode=daily',
                 ],
             ]);
         }
+
         if ($image) {
             $file = explode('/', $url);
             $file = array_pop($file);
@@ -61,15 +115,5 @@ class Pixiv
             return file_put_contents($file, $image) !== false ? $file : false;
         }
         return false;
-    }
-
-    /**
-     * 执行刷新线程
-     */
-    public static function runRefreshThread()
-    {
-        Curl::get(Config::$url . 'index.php?job=refresh', [
-            CURLOPT_TIMEOUT => 1,
-        ]);
     }
 }
