4580ac3c40adf00a093c797567601d7df8e55827
mokeyjay <m@html.love>
2020-02-05 19:29:26 +0800

    service 配置项的取值不再影响 limit 配置项；提升 suning 图床的推荐顺序

--- config.php
+++ config.php
@@ -16,7 +16,7 @@ return [
     'background_color' => 'transparent',
 
     /**
-     * 显示和缓存的图片数量限制（范围1-50）
+     * 显示和缓存的图片最大数量（范围1-500）
      * 例如将此值设为 10 则可以做出 Top10 的效果
      * 也可防止部分辣鸡主机在缓存图片时占用过多资源导致卡死或报警
      * 一般情况下默认的 50 就行
@@ -25,9 +25,7 @@ return [
 
     /**
      * 是否对外提供服务
-     * 为 true 时，任何人都可通过 url 的 get参数 来临时修改 background_color 和 limit 的值
-     * 且将强制缓存 50 张缩略图。不受上面的 limit 限制
-     * （避免出现你设定的 limit 小于他人请求的 limit 的情况）
+     * 为 true 时，任何人都可通过 url 的 get 参数来临时修改 background_color 和 limit 的值
      */
     'service' => true,
 
@@ -55,21 +53,24 @@ return [
 
     /**
      * 图床名称
-     * 可多选：jd、netease、smms、suning、imgsb、local、tietuku
-     * jd=京东、netease=网易、smms=Sm.ms图床、suning=苏宁、imgsb=smms图床v2(内测中)、local=服务器本地、tietuku=贴图库
+     * 可多选：jd、netease、smms、suning、local、tietuku
+     * jd=京东、netease=网易、suning=苏宁、smms=Sm.ms图床、local=服务器本地、tietuku=贴图库
      * （推荐度按照顺序从高到低）
      *
      * 推荐填写多个图床，如果其中一个图床上传失败，则将按照顺序继续尝试其他图床
      * （因jd图床对图片尺寸有最小限制，部分较小的缩略图会上传失败，因此强烈不建议单独使用jd图床）
      */
-    'image_hosting' => ['jd', 'netease', 'smms', 'suning', 'local'],
+    'image_hosting' => ['jd', 'netease', 'suning', 'smms', 'local'],
 
     /**
-     * tietuku 图床需要到 http://www.tietuku.com/manager/createtoken 注册登录后生成token填到下方才能使用
+     * 图床扩展配置信息
      */
     'image_hosting_extend' => [
         'tietuku' => [
             'token' => ''
-        ]
+        ],
+        'smms' => [
+            'token' => '',
+        ],
     ]
 ];
