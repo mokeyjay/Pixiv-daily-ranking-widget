02fd456dd53ffb582f52409e65ac2b9d5d7acb06
mokeyjay <i@mokeyjay.com>
2022-05-05 00:20:57 +0800

    优化 载入中 页面

--- app/Views/loading.php
+++ app/Views/loading.php
@@ -11,38 +11,51 @@ use app\Libs\Config;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Pixiv 每日排行榜 Top<?=Config::$limit?> 小挂件</title>
-
-  <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.4.1/css/bootstrap.min.css">
   <style>
     html, body { height : 100%; background-color : #<?=Config::$background_color?>; }
-
-    body { display : flex; align-items : center; justify-content : center; }
-
+    body { display: flex; align-items: center; justify-content: center; margin: 0; }
     div { text-align : center; }
 
-    .glyphicon { display : block; font-size : 48px; color : #888888; margin-bottom : 20px; }
-
-    .tip { font-size : 22px; color : #aaaaaa; }
-
-    .circle { animation : circle 3s infinite linear; }
-
-    @-webkit-keyframes circle {
-      0% { transform : rotate(0deg); }
-      100% { transform : rotate(360deg); }
+    @keyframes spinner-two-alt {
+      0% {transform: rotate(0deg)}
+      to {transform: rotate(359deg)}
     }
+
+    .gg-spinner-two-alt,.gg-spinner-two-alt::before {
+      box-sizing: border-box;
+      display: block;
+      width: 40px;
+      height: 40px;
+      color: #aaaaaa;
+    }
+
+    .gg-spinner-two-alt {
+      position: relative;
+      margin: 20px auto;
+    }
+
+    .gg-spinner-two-alt::before {
+      content: "";
+      position: absolute;
+      border-radius: 100px;
+      animation: spinner-two-alt 1s cubic-bezier(.6,0,.4,1) infinite;
+      border: 6px solid transparent;
+      border-bottom-color: currentColor;
+      border-top-color: currentColor
+    }
+
+    .tip { font-size: 22px; color: #aaaaaa; }
   </style>
   <script>
     setTimeout(function() {
       location.reload()
-    }, 10000)
+    }, 1000000)
   </script>
 </head>
 <body>
 <div>
-  <span class="glyphicon glyphicon-refresh circle" aria-hidden="true"></span>
+  <i class="gg-spinner-two-alt"></i>
   <div class="tip">排行榜更新中<br>请稍候</div>
 </div>
 </body>
-<script src="https://cdn.staticfile.org/jquery/2.2.4/jquery.min.js"></script>
-<script src="https://cdn.staticfile.org/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </html>
