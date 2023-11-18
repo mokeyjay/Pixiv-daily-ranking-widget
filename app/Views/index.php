99f19995f34efc926c4b18853462c093f7ce0c01
mokeyjay <ggmokeyjay@gmail.com>
2023-11-18 15:33:48 +0800

    重写的前端

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,11 +1,7 @@
-<?php
-
-use app\Libs\Config;
-
-?>
 <!-- 来自 mokeyjay 的 Pixiv每日排行榜小挂件 -->
 <!-- 博客：https://www.mokeyjay.com -->
-<!-- 这个博客将会集技术、ACG、日常、分享于一身，如果你喜欢，常来玩哦 -->
+<!-- 这个博客将会集技术、ACG、日常、分享于一身。如果你喜欢，常来玩哦 -->
+
 <!doctype html>
 <html lang="zh-CN">
 <head>
@@ -13,255 +9,260 @@ use app\Libs\Config;
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta name="referrer" content="no-referrer">
-  <title>Pixiv 每日排行榜 Top<?= Config::$limit ?> 小挂件</title>
+  <title>Pixiv 每日排行榜 Top<?= app\Libs\Config::$limit ?> 小挂件</title>
 
   <style>
-    * { margin: 0; padding: 0; }
-    html, body, #container, ul, li, a { height: 100%; }
-    body {
-      background: <?=Config::$background_color?>;
-    }
+    * { margin: 0; padding: 0; outline: none; }
 
-    #container {
+    .carousel {
       overflow: hidden;
     }
-
-    /* 图片列表 & 图片 */
-    ul {
-      list-style: none;
-      transition: ease-in-out transform .5s;
-    }
-    li {
-      float: left;
+    .list {
+      height: 100vh;
       position: relative;
-      overflow: hidden;
     }
-    a {
+    .list-item {
+      position: absolute;
+      width: 100%;
       display: flex;
-      justify-content: center;
+      align-items: center;
+      height: 100%;
+      background-color: #fff;
     }
     img {
-      object-fit: contain;
-      width: 100%;
+      width: 100vw;
+    }
+
+    .list-item.current {
+      z-index: 2;
+    }
+
+    /* 翻页动画 */
+    .current-to-prev {
+      z-index: 1;
+      animation: slide-current-to-prev .5s cubic-bezier(0.34, 0.69, 0.1, 1);
+    }
+    @keyframes slide-current-to-prev {
+      from { transform: translateX(0) }
+      to { transform: translateX(-100vw) }
+    }
+    .current-to-next {
+      z-index: 1;
+      animation: slide-current-to-next .5s cubic-bezier(0.34, 0.69, 0.1, 1);
+    }
+    @keyframes slide-current-to-next {
+      from { transform: translateX(0) }
+      to { transform: translateX(100vw) }
+    }
+    .next-to-current {
+      z-index: 1;
+      animation: slide-next-to-current .5s cubic-bezier(0.34, 0.69, 0.1, 1);
+    }
+    @keyframes slide-next-to-current {
+      from { transform: translateX(100vw) }
+      to { transform: translateX(0) }
+    }
+    .prev-to-current {
+      z-index: 1;
+      animation: slide-prev-to-current .5s cubic-bezier(0.34, 0.69, 0.1, 1);
+    }
+    @keyframes slide-prev-to-current {
+      from { transform: translateX(-100vw) }
+      to { transform: translateX(0) }
     }
 
     /* 左右翻页按钮 */
     .button {
-      cursor: pointer;
-      position: fixed;
-      top: 0;
       height: 100%;
       width: 70px;
+      position: fixed;
+      top: 0;
+      z-index: 5;
+      cursor: pointer;
       display: flex;
       justify-content: center;
       align-items: center;
-      transition: ease-in-out opacity .3s,
-                  transform .5s;
+      transition: transform .3s ease-in-out, opacity .5s ease-in-out;
       opacity: 0;
     }
     .button.prev {
-      left: -70px;
+      left: 0;
+      transform: translateX(-70px);
     }
     .button.next {
-      right: -70px;
+      right: 0;
+      transform: translateX(70px);
     }
-    body:hover .button { opacity: 1; }
-    body:hover .button.prev { transform: translateX(70px); }
-    body:hover .button.next { transform: translateX(-70px); }
-    /* 左右箭头 */
+    .button .arrow {
+      transform: rotate(45deg);
+      margin-top: -16px;
+    }
     .button i {
       position: relative;
-      display: block;
-      width: 30px;
-      height: 30px;
     }
-    .button i::before, .button i::after {
+    .button.prev i:before, .button.prev i:after, .button.next i:before, .button.next i:after {
       display: block;
-      content: " ";
-      width: 16px;
-      height: 16px;
+      content: "";
       border: 4px solid white;
       border-radius: 2px;
       position: absolute;
+      width: 16px;
+      height: 16px;
+    }
+    .button.prev i:before, .button.prev i:after {
+      border-top: none;
+      border-right: none;
+    }
+    .button.next i:before, .button.next i:after {
       border-bottom: none;
       border-left: none;
-      transform: rotate(45deg);
     }
-    .button i::before {
-      border-color: #777;
-      filter: blur(2px);
+    .button i:before {
+      filter: blur(4px);
+      border-color: #777777 !important;
     }
-    .button.prev i::before, .button.prev i::after {
-      transform: rotate(225deg);
-      left: 10px;
+    .carousel:hover .button {
+      transform: translateX(0);
+      opacity: 1;
     }
-    /*.button.prev i::before, .button.prev i::after {*/
-    /*  border-top: none;*/
-    /*  border-right: none;*/
-    /*}*/
 
-    /* 阴影遮罩层 */
     .mask {
-      width: 100%;
       height: 150px;
-      transform: translateY(150px);
+      width: 100%;
+      position: fixed;
       bottom: 0;
-      position: absolute;
-      pointer-events: none;
-      transition-duration: .5s;
-      background-image: linear-gradient(transparent, rgba(0, 0, 0, 0.4));
+      background: linear-gradient(to top, rgba(0, 0, 0, .2), transparent);
+      transform: translateY(150px);
+      transition: transform .3s ease-in-out
     }
-    body:hover .mask { bottom: 0; transform: translateY(0); }
+    .carousel:hover .mask {
+      transform: translateY(0);
+    }
 
     /* 作品 & 作者信息 */
     .info {
       opacity: 0;
-      transition-duration: .5s;
       text-shadow: black 0.1em 0.1em 0.2em;
       pointer-events: none;
       bottom: 60px;
       position: fixed;
       text-align: center;
       color: white;
+      width: 100%;
+      transition: opacity .3s ease-in-out;
     }
     body:hover .info { opacity: 1 }
     .title {
-      font-size: 1rem;
+      font-size: 1.2rem;
       white-space: nowrap;
       text-overflow: ellipsis;
     }
     .author {
-      font-size: .75rem;
+      margin-top: 5px;
+      font-size: 1rem;
     }
+
   </style>
   <script>
-      <?= Config::$header_script ?>
+      <?= app\Libs\Config::$header_script ?>
   </script>
 </head>
 <body>
-<div id="container">
-  <?php
-    // 每个 li 的宽度
-    $perWidth = 100 / (Config::$limit + 2);
+  <div class="carousel">
+    <div class="list">
+      <?php foreach ($pixivJson['data'] as $k => $data): ?>
+        <div class="list-item <?= $k === 0 ? 'current' : '' ?>">
+          <a href="https://www.pixiv.net/artworks/<?= $data['id'] ?>" target="_blank">
+            <img src="<?= $k === 0 ? $data['url'] : '' ?>" data-src="<?= $data['url'] ?>" alt="<?= htmlentities($data['title']) ?>">
+            <div class="mask"></div>
+            <div class="info">
+              <div class="title"><?= htmlentities($data['title']) ?></div>
+              <div class="author"><?= htmlentities($data['user_name']) ?></div>
+            </div>
+          </a>
+        </div>
+      <?php endforeach; ?>
+    </div>
+    <div class="control">
+      <div class="button prev" onclick="switchPage('prev')">
+        <div class="arrow"><i></i></div>
+      </div>
+      <div class="button next" onclick="switchPage('next')">
+        <div class="arrow"><i></i></div>
+      </div>
+    </div>
+  </div>
 
-    $pixivJson['data'] = array_slice($pixivJson['data'], 0, Config::$limit);
+  <script>
+    const $ = document.querySelector.bind(document);
+    const $$ = document.querySelectorAll.bind(document);
 
-    // 在图片列表最前面加上最后一张图、最后面加上最前一张图，确保边界情况下左右翻页动画过渡效果正常
-    $firstImage = $pixivJson['data'][0];
-    $lastImage = $pixivJson['data'][count($pixivJson['data']) - 1];
-    $pixivJson['data'] = array_merge([$lastImage], $pixivJson['data'], [$firstImage]);
-  ?>
+    function switchPage(direction) {
 
-  <ul id="list" style="width: <?= (Config::$limit + 2) * 100 ?>%; transform: translateX(-<?= $perWidth ?>%)">
-    <?php foreach ($pixivJson['data'] as $k => $data): ?>
-      <li data-index="<?= $k ?>" style="width: <?= $perWidth ?>%">
-        <a href="https://www.pixiv.net/artworks/<?= $data['id'] ?>" target="_blank">
-          <img src="<?= $k === 1 ? $data['url'] : '' ?>" data-src="<?= $data['url'] ?>" alt="<?= htmlentities($data['title']) ?>">
-          <div class="mask"></div>
-          <div class="info">
-            <div class="title"><?= htmlentities($data['title']) ?></div>
-            <div class="author"><?= htmlentities($data['user_name']) ?></div>
-          </div>
-        </a>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+      const currentDom = $('.list-item.current');
+      const nextDom = findNextDomByDirection(currentDom, direction);
 
-  <div class="button prev" onclick="switchPage('left')"><i></i></div>
-  <div class="button next" onclick="nextPage()"><i></i></div>
-</div>
+      [currentDom, nextDom].map(dom => {
+        dom.addEventListener('animationend', function end() {
+          if (dom.classList.contains('next-to-current') || dom.classList.contains('prev-to-current')) {
+            dom.className = 'list-item current'
+          } else {
+            dom.className = 'list-item'
+          }
 
-<script>
-  let page    = 1;
-  let maxPage = <?= Config::$limit ?>;
-  let perWidth = <?= $perWidth ?>;
-  let list     = document.getElementById('list')
+          dom.removeEventListener('animationend', end)
+        })
+      })
 
-  // 加载图片
-  function loadImages(indexes) {
-    for (let i of indexes) {
-      let img = document.querySelector(`li[data-index='${i}'] img`)
-      img && img.setAttribute('src', img.getAttribute('data-src'))
-    }
-  }
-
-  // 翻页锁
-  let switchLock = false
-  // 翻页
-  function switchPage(action) {
-    if (switchLock) return
-
-    switchLock = true
-    action === 'left' ? page-- : page++;
-
-    // 图片预加载
-    // 一般情况下只需要根据翻页方向预加载下一页就够了
-    if (page >= 1 && page <= maxPage) {
-      loadImages([action === 'left' ? page - 1 : page + 1])
-    } else if (page === 0) {
-      // 但在左翻到第 0 页（即排行榜最后一张图）时，需要同时预加载最后一张图和倒数第二张图
-      loadImages([maxPage, maxPage - 1])
-    }
-
-    list.style.transform = 'translateX(-' + (page * perWidth) + '%)';
-
-    // 如果列表已经达到边界，就重置列表位置
-    if (page === 0 || page === maxPage + 1) {
-      list.addEventListener('transitionend', resetListPosition)
-    }
-  }
-  list.addEventListener('transitionend', () => switchLock = false)
-
-  // 重置列表位置
-  function resetListPosition() {
-    // 翻到第 0 页了，要关闭动画瞬间跳回列表最末尾，不然没法继续往左翻
-    // 翻到最后一页也是同理
-    page = page === 0 ? maxPage : 1
-    list.style.transition = 'none'
-    list.style.transform = 'translateX(-' + (page * perWidth) + '%)'
-
-    setTimeout(() => {
-      list.style.transition = 'ease-in-out all .5s'
-    })
-
-    list.removeEventListener('transitionend', resetListPosition)
-  }
-
-  function nextPage() {
-    switchPage('right')
-  }
-
-  // 自动播放
-  let interval = null
-  function startAutoPlay() {
-    clearInterval(interval)
-    interval = setInterval(nextPage, 5000)
-  }
-
-  window.addEventListener('DOMContentLoaded', () => {
-
-    loadImages([0, 1, 2, maxPage + 1])
-
-    // 注册自动滚动
-    startAutoPlay()
-    document.addEventListener('pointerenter', () => clearInterval(interval))
-    document.addEventListener('pointerleave', startAutoPlay)
-
-    // 左右滑动手势
-    let startX = 0
-    document.addEventListener('touchstart', e => startX = e.changedTouches[0].pageX);
-    document.addEventListener('touchend', e => {
-      let endX = e.changedTouches[0].pageX;
-      let diffX = endX - startX;
-
-      if (diffX > 50) {
-        switchPage('left')
-      } else if (diffX < -50) {
-        nextPage()
+      if (direction === 'next') {
+        currentDom.className = 'list-item current-to-prev'
+        nextDom.className = 'list-item next-to-current'
+      } else {
+        currentDom.className = 'list-item current-to-next'
+        nextDom.className = 'list-item prev-to-current'
       }
-    });
-  })
 
-</script>
+      // 预加载下一张图
+      loadImage(findNextDomByDirection(nextDom, direction))
+    }
+
+    // 根据翻页方向获取下一个 dom
+    function findNextDomByDirection(currentDom, direction) {
+      let prop = direction === 'next' ? 'next' : 'previous'
+      let nextDom = currentDom[`${prop}ElementSibling`]
+
+      // 对应方向找不到下一个 dom，就说明滑动到尽头了，要跳到开头或结尾
+      if (nextDom === null) {
+        prop = direction === 'next' ? 'first' : 'last'
+        nextDom = $('.list').querySelector(`.list-item:${prop}-child`)
+      }
+
+      return nextDom
+    }
+
+    // 加载图片
+    function loadImage() {
+      for (let dom of arguments) {
+        let img = dom.querySelector('img')
+        img.setAttribute('src', img.getAttribute('data-src'))
+      }
+    }
+
+    let interval = null
+    function registerAutoPlay() {
+      clearInterval(interval)
+      interval = setInterval(() => switchPage('next'), 5000)
+    }
+
+    (function() {
+
+      // 预加载前后两张图
+      const currentDom = $('.list-item.current');
+      loadImage(findNextDomByDirection(currentDom, 'prev'), findNextDomByDirection(currentDom, 'next'))
+
+      registerAutoPlay()
+    })()
+
+  </script>
 </body>
 </html>
