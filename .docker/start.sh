eedacaedae132f546b21fe043c1129422e599369
mokeyjay <ggmokeyjay@gmail.com>
2023-11-24 20:42:06 +0800

    修复环境变量存在空格时定时任务失效的问题

--- .docker/start.sh
+++ .docker/start.sh
@@ -2,7 +2,9 @@
 
 echo '' > /etc/nginx/sites-enabled/default
 
-printenv > /var/www/html/.docker/env.sh
+# 将环境变量保存起来，免得 crontab 读不到
+# 部分环境变量值含有空格，得用双引号包起来，不然 source 时会报错
+printenv | awk -F= -v OFS== '{ if ($2 ~ /[[:space:]]/) $2="\""$2"\""; print }' > /var/www/html/.docker/env.sh
 
 chown -R www-data:www-data /var/www/html
 
